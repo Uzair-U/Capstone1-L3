551c5a1a6ad741f28f4b8b5fcfbc4f4cc5526bf1
Uzair <uzairsma1l@gmail.com>
2023-05-01 11:49:56 +0200

    Changed conf.py file to include project.

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,10 @@
 # Configuration file for the Sphinx documentation builder.
+import os
+import sys
+import django
+sys.path.insert(0, os.path.abspath('..'))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'GetIt.settings'
+django.setup()
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
@@ -24,5 +30,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
